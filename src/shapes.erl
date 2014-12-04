536caf6ce85d99288e8760701f16d721c006741a
Tomas Vestelind <tomas.vestelind@hiq.se>
2014-12-04 15:52:24 +0100

    Added tringle to the shapes module

--- src/shapes.erl
+++ src/shapes.erl
@@ -1,9 +1,13 @@
 -module(shapes).
 -export([area/1, perimiter/1]).
+-import(math,[]).
 -include("shapes.hrl").
 
 area(#circle{radius = R}) -> R * R * 3.14;
-area(#rectangle{width = W, length = L}) -> W * L.
+area(#rectangle{width = W, length = L}) -> W * L;
+area(#triangle{a = S1, b = S2, c = S3}) -> S = (S1 + S2 + S3) / 2,
+                                           math:sqrt(S * (S - S1) * (S - S2) * (S - S3)).
 
 perimiter(#circle{radius = R}) -> 2 * R * 3.14;
-perimiter(#rectangle{width = W, length = L}) -> 2 * (W + L).
+perimiter(#rectangle{width = W, length = L}) -> 2 * (W + L);
+perimiter(#triangle{a = S1, b = S2, c = S3}) -> S1 + S2 + S3.
--- src/shapes.hrl
+++ src/shapes.hrl
@@ -1,2 +1,3 @@
 -record(circle, {radius}).
 -record(rectangle, {width, length}).
+-record(triangle, {a, b, c}).
