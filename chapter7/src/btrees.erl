81b89700c6ba4c776f604cea8154ee3097cd7a63
Tomas Vestelind <tomas.vestelind@hiq.se>
2014-12-10 17:40:00 +0100

    Starting using edoc and type specifications

--- chapter7/src/btrees.erl
+++ chapter7/src/btrees.erl
@@ -1,35 +1,62 @@
+%% @author Tomas Vestelind
+%% @doc This modules provides basic functionality on binary trees.
+%%      A binary tree is a node that may be connected to up to 2 other
+%%      nodes. Each node must also contains a value, which by default is 0.
+%%
+
 -module(btrees).
--export([sum/1, max/1, min/1, is_ordered/1, insert/2, gentree/1, test_list/0]).
+-export([sum/1, max/1, min/1, is_ordered/1, insert/2]).
 -import(lists,[]).
 -include("btrees.hrl").
 
+%% @doc Find the largest value stored in the tree.
+-spec(max(Tree::#btree{}) -> integer()).
+
+max(#btree{left = empty, right = empty, value = V}) -> V;
+max(#btree{left = L, right = empty, value = V}) -> erlang:max(V, max(L));
+max(#btree{left = empty, right = R, value = V}) -> erlang:max(V, max(R));
+max(#btree{left = L, right = R, value = V}) -> max3(V, max(L), max(R)).
+
+%% @doc Find the smallest value stored in the tree.
+-spec(min(Tree::#btree{}) -> integer()).
+
+min(#btree{left = empty, right = empty, value = V}) -> V;
+min(#btree{left = L, right = empty, value = V}) -> erlang:min(V, min(L));
+min(#btree{left = empty, right = R, value = V}) -> erlang:min(V, min(R));
+min(#btree{left = L, right = R, value = V}) -> min3(V, min(L), min(R)).
+
+%% @doc Sum all values in the tree.
+-spec(sum(Tree::#btree{}) -> integer()).
+
 sum(#btree{left = empty, right = empty, value = V}) -> V;
-sum(#btree{left = L, right = empty, value = V}) -> V + sum(L); 
+sum(#btree{left = L, right = empty, value = V}) -> V + sum(L);
 sum(#btree{left = empty, right = R, value = V}) -> V + sum(R);
 sum(#btree{left = L, right = R, value = V}) -> V + sum(L) + sum(R).
 
-max(#btree{left = empty, right = empty, value = V}) -> V;
-max(#btree{left = L, right = empty, value = V}) -> max(V, max(L));
-max(#btree{left = empty, right = R, value = V}) -> max(V, max(R));
-max(#btree{left = L, right = R, value = V}) -> max3(V, max(L), max(R)).
-
-min(#btree{left = empty, right = empty, value = V}) -> V;
-min(#btree{left = L, right = empty, value = V}) -> min(V, min(L));
-min(#btree{left = empty, right = R, value = V}) -> min(V, min(R));
-min(#btree{left = L, right = R, value = V}) -> min3(V, min(L), min(R)).
+%% @doc Check if the tree is ordered such that for each node, all nodes to the
+%%      left contain lower or equal values and all nodes to the right contain
+%%      larger values.
+-spec(is_ordered(Tree::#btree{}) -> boolean()).
 
 is_ordered(#btree{left = empty, right = empty}) -> true;
 is_ordered(#btree{left = L, right = empty, value = V}) -> is_ordered(L) and (max(L) =< V);
-is_ordered(#btree{left = empty, right = R, value = V}) -> is_ordered(R) and (V =< min(R));
-is_ordered(#btree{left = L, right = R, value = V}) -> is_ordered(L) and is_ordered(R) and (max(L) =< V) and (V =< min(R)).
+is_ordered(#btree{left = empty, right = R, value = V}) -> is_ordered(R) and (V < min(R));
+is_ordered(#btree{left = L, right = R, value = V}) -> is_ordered(L) and is_ordered(R) and (max(L) =< V) and (V < min(R)).
+
+%% @doc Insert an element into an already ordered tree such that the tree
+%%      remains ordered according to the rules in @see is_ordered/1.
+-spec(insert(Element::#btree{}, Tree::#btree{}) -> NewTree::#btree{}).
 
 insert(E, empty) -> E;
 insert(#btree{value = Vnew} = E, #btree{left = L, value = V} = T) when Vnew =< V -> T#btree{left = insert(E, L)};
 insert(#btree{value = Vnew} = E, #btree{right = R, value = V} = T) when V < Vnew -> T#btree{right = insert(E, R)}.
-                                                                        
-test_list() -> [#btree{value=1}, #btree{value=2}, #btree{value=4}, #btree{value=3}, #btree{value=2}, #btree{value=28}, #btree{value=100}, #btree{value=-1}, #btree{value=-100}, #btree{value=2001}, #btree{value=162000}, #btree{value=17}, #btree{value=29}, #btree{value=18}, #btree{value=3}, #btree{value=6}, #btree{value=3}, #btree{value=-100}].
-gentree(List) -> lists:foldr(fun(E, Acc) -> insert(E, Acc) end, empty, List). 
 
 % Internal functions
+
+-spec(max3(integer(), integer(), integer()) -> integer()).
+
 max3(A,B,C) -> max(A, max(B, C)).
+
+-spec(min3(integer(), integer(), integer()) -> integer()).
+
 min3(A,B,C) -> min(A, min(B, C)).
